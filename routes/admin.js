2f668fe962786a6b4383c44349a77f30760ddaf7
amreeth <amreethashok@gmail.com>
2022-01-10 17:55:58 +0530

    second

--- routes/admin.js
+++ routes/admin.js
@@ -27,6 +27,78 @@ router.get('/', function(req, res, next) {
       category:"mobile",
       price:'$600',
       image:"https://i02.appmifile.com/362_operator_in/23/04/2021/b23987f4a6605e2f4be4562bd0149fd8.png"
+    },
+    {
+      name:"Samsung M52 5G ",
+      category:"mobile",
+      price:'$320',
+      image:"https://images.samsung.com/is/image/samsung/p6pim/in/sm-m526bzkiinu/gallery/in-galaxy-m52-5g-m526-404058-sm-m526bzkiinu-514948644?$684_547_PNG$"
+    },
+    {
+      name:"Vivo X60",
+      category:"mobile",
+      price:'$290',
+      image:"https://m.media-amazon.com/images/I/71YD6OR5IZL._SL1200_.jpg"
+    },
+    {
+      name:"OPPO F19",
+      category:"mobile",
+      price:'$250',
+      image:"https://m.media-amazon.com/images/I/712PBW8cGBL._SL1500_.jpg"
+    },
+    {
+      name:"Mi 11 Ultra ",
+      category:"mobile",
+      price:'$600',
+      image:"https://i02.appmifile.com/362_operator_in/23/04/2021/b23987f4a6605e2f4be4562bd0149fd8.png"
+    },
+    {
+      name:"IQOO 7",
+      category:"mobile",
+      price:'$350',
+      image:"https://m.media-amazon.com/images/I/51OvvRTnJTS._SL1200_.jpg"
+    },
+    {
+      name:"OPPO Reno6 5G",
+      category:"mobile",
+      price:'$300',
+      image:""
+    },
+    {
+      name:"One plus Nord 2 5G",
+      category:"mobile",
+      price:'$290',
+      image:"https://m.media-amazon.com/images/I/71KVeQql77S._SL1500_.jpg"
+    },
+    {
+      name:"Redmi 10 Prime",
+      category:"mobile",
+      price:'$150',
+      image:"https://m.media-amazon.com/images/I/817clKAKcqL._SL1500_.jpg"
+    },
+    {
+      name:"Tecno Spark 8 Pro",
+      category:"mobile",
+      price:'$130',
+      image:"https://m.media-amazon.com/images/I/81SCv8+cG1L._SL1500_.jpg"
+    },
+    {
+      name:"Samsung M12",
+      category:"mobile",
+      price:'$130',
+      image:"https://m.media-amazon.com/images/I/7162Y5fPdkL._SL1500_.jpg"
+    },
+    {
+      name:"ASUS ROG 5",
+      category:"mobile",
+      price:'$340',
+      image:""
+    },
+    {
+      name:"Nokia C01",
+      category:"mobile",
+      price:'$100',
+      image:"https://m.media-amazon.com/images/I/61CXAHkNXCL._SL1080_.jpg"
     }
   ]
   res.render('admin/view-products',{admin:true,products})
